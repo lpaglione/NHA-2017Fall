f52f6ea091b34605e102979f614861d11878c0c5
lpaglione <l.paglione@orcid.org>
2017-09-10 13:32:56 -0400

    Challenge 3: getting and setting text
    Create a small app to get someone’s name and then say hello once the
    name is submitted

--- resources/js/introChallenges.js
+++ resources/js/introChallenges.js
@@ -4,4 +4,8 @@
 // September, 2017
 // Introductory challenges - javascript code
 
-alert("WOW! that worked!");
+//Challenge 3 code
+$("button#challenge3").click(() => {
+    let name = $("input#challenge3").val();
+    $("p#challenge3").html(`Nice to meet you, ${name}! `);
+});
