120de70a92578516fd42d16afbbda3ddf4ce0127
lpaglione <l.paglione@orcid.org>
2017-09-12 17:43:18 -0400

    Fixing syntax
    Should be const instead of let because I’m not changing the value
    within the function. The variable will be released when the function is
    done executing.

--- resources/js/introChallenges.js
+++ resources/js/introChallenges.js
@@ -6,15 +6,15 @@
 
 //Challenge 3 code
 $("button#challenge3").click(() => {
-  let name = $("input#challenge3").val();
+  const name = $("input#challenge3").val();
   $("p#challenge3").html(`Nice to meet you, ${name}! `);
 });
 
 //Challenge 4 code
 $("button#challenge4").click(() => {
-  let name = $("input#challenge4Name").val();
-  let food = $("input#challenge4Food").val();
-  let color = $("input#challenge4Color").val();
+  const name = $("input#challenge4Name").val();
+  const food = $("input#challenge4Food").val();
+  const color = $("input#challenge4Color").val();
   $("table#challenge4").append(`<tr><td>${name}</td><td>${food}</td><td bgcolor=${color}></td></tr>`);
 });
 
